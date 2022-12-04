999f9154ae7b41102962be7c6fdab69fd8cfc12f
AniaPiWo <snowflake@wp.pl>
2022-12-04 12:38:58 +0100

    3 button fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -14,24 +14,28 @@ function onSubmit(number) {
   let step = Number(number.currentTarget.step.value);
   let amount = Number(number.currentTarget.amount.value);
 
-  for (let position = 1; position <= amount; position += 1) {
+   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-      });
+      })
 
     delay += step;
-  }
+  } 
 
   number.currentTarget.reset();
+
+  startBtn.disabled = true;
+  setTimeout(function () {
+    startBtn.disabled = false;
+  }, delay)
+
 }
 
 function createPromise(position, delay) {
-
-  startBtn.disabled = true;
 
   return new Promise((resolve, reject) => {
 
@@ -44,5 +48,5 @@ function createPromise(position, delay) {
       }
     }, delay);
   })
-
 };
+
