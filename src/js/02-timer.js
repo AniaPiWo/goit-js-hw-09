928e318d010b72987377dfe571cc95984b230f4e
AniaPiWo <snowflake@wp.pl>
2023-11-26 18:21:00 +0100

    timer fix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,13 +5,17 @@ import '../css/common.css';
 
 const input = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('#start');
+const stopBtn = document.querySelector('#stop');
 const daysOutput = document.querySelector('#days');
 const hoursOutput = document.querySelector('#hours');
 const minutesOutput = document.querySelector('#minutes');
 const secondsOutput = document.querySelector('#seconds');
 
 let selectedDate = null;
+let timerId = null; // Declare timerId outside the click event to access it later
+
 startBtn.disabled = true;
+stopBtn.disabled = true;
 
 function convertMs(ms) {
   const second = 1000;
@@ -31,21 +35,23 @@ const selectedDateUpdate = () => {
     selectedDate - Date.now()
   );
   daysOutput.textContent = days;
-  hoursOutput.textContent = hours;
-  minutesOutput.textContent = minutes;
-  secondsOutput.textContent = seconds;
+  hoursOutput.textContent = hours.toString().padStart(2, '0');
+  minutesOutput.textContent = minutes.toString().padStart(2, '0');
+  secondsOutput.textContent = seconds.toString().padStart(2, '0');
 };
 
 const counterReset = () => {
+  clearInterval(timerId);
   daysOutput.textContent = '0';
   hoursOutput.textContent = '00';
   minutesOutput.textContent = '00';
   secondsOutput.textContent = '00';
 };
 
-//style customization
+// Style customization
 input.classList.add('flatpickr-input');
-//date picker
+
+// Date picker
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -53,14 +59,10 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     selectedDate = selectedDates[0];
-    console.log(selectedDate);
     if (Date.now() < selectedDate) {
       startBtn.disabled = false;
       selectedDateUpdate();
-    }
-
-    if (Date.now() > selectedDate) {
-      defaultDate = Date.now();
+    } else {
       startBtn.disabled = true;
       Notify.failure('Please choose a date in the future');
     }
@@ -70,10 +72,9 @@ const options = {
 flatpickr(input, options);
 
 startBtn.addEventListener('click', () => {
-  startBtn.disabled = true;
   Notify.success('Countdown starts!');
 
-  const timerId = setInterval(() => {
+  timerId = setInterval(() => {
     selectedDateUpdate();
     if (Date.now() > selectedDate) {
       clearInterval(timerId);
@@ -81,4 +82,18 @@ startBtn.addEventListener('click', () => {
       Notify.success('Countdown finished!');
     }
   }, 1000);
+
+  input.disabled = true;
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    counterReset();
+    selectedDate = null;
+    Notify.failure('Countdown interrupted!');
+    input.disabled = false;
+    startBtn.disabled = true;
+    stopBtn.disabled = true;
+  });
 });
